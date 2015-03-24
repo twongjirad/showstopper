3ad91c188b79a1adc40d954a152171eb707fb709
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2015-03-24 01:28:25 -0400

    cleaned wire plotter

--- wire_plot_fft.py
+++ wire_plot_fft.py
@@ -4,8 +4,10 @@ from vireviewer import getmw
 import numpy as np
 import pandas as pd
 from channelmap import getChannelMap
+from hoot import gethootdb
 from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph as pg
+import math
 
 def get_pulsed_list(run):
     f = open('runmeta/run%03d_pulsed_ch.txt'%(run),'r')
@@ -42,8 +44,6 @@ def plot_run( mw, run, subrun1, subrun2, plotfft=True, subbg=True ):
                 col = "bg_"+col
             bg_cols.append( col )
         bgdf.columns = bg_cols
-        gbgdf = bgdf.groupby( ('crate','slot','femch') )
-        bgdf2 = pd.DataFrame( gbgdf.mean() )
 
     # open data we are focuing on
     npzfile = np.load( 'output/run%03d_subrun%03d_%03d.npz'%(run,subrun1,subrun2) )
@@ -51,17 +51,23 @@ def plot_run( mw, run, subrun1, subrun2, plotfft=True, subbg=True ):
     df = pd.DataFrame( arr )
     df.drop( 'index', axis=1, inplace=True )
     if subbg:
-        gdf = df.groupby( ('crate','slot','femch') )
-        df2 = pd.DataFrame( gdf.mean() )
+        df.set_index(['crate','slot','femch'],inplace=True)
+        bgdf.set_index(['crate','slot','femch'],inplace=True)
+        df = df.join( bgdf )
 
-        # join the data table
-        df = df2.join( bgdf2 ).reset_index()
+    hootdb = gethootdb()
+    chmap = getChannelMap()
+    hootdb.set_index( ['crate','slot','femch'], inplace=True )
+    df = df.join( hootdb ).reset_index()
 
-    arr = df.to_records()
+    # now have supertable
+    print df.columns
+    print len(df)
+    print len(chmap)
 
     maxamp = np.max( df['max_amp'].values )
+    arr = df.to_records()
 
-    chmap = getChannelMap()
     print pulsed_list[0]
     pulsed_row = df.query( '(crate==%d) & (slot==%d) & (femch==%d)'%(pulsed_list[0][0], pulsed_list[0][1],pulsed_list[0][2]) )
     max_ampratio = pulsed_row['max_amp'].values[0]/pulsed_row['ped_rms'].values[0]
@@ -93,11 +99,17 @@ def plot_run( mw, run, subrun1, subrun2, plotfft=True, subbg=True ):
     candidates = []
 
     for r in arr:
-        row = chmap.query( '(crate==%d) & (slot==%d) & (femch==%d)'%(r['crate'],r['slot'],r['femch']) )
+
+        if math.isnan(r['wireid']):
+            #print "skipping: ",r['crate'],r['slot'],r['femch']
+            mw.vires.setWireColorByCSF( r['crate'],r['slot'],r['femch'], (0.01, 0.01, 0.01, 0.01) )
+            continue
 
         pulsed = False
         if [r['crate'],r['slot'],r['femch']] in pulsed_list:
             pulsed = True
+
+        # hack to fix unknown problem
         if (r['crate'],r['slot'],r['femch'])==(1,8,0):
             #mw.vires.setWireColorByCSF( r['crate'],r['slot'],r['femch'], (0.01, 0.01, 0.01, 0.01) )
             mw.vires.setWireColor( 'U',640, (0.01, 0.01, 0.01, 0.05) )
@@ -105,69 +117,55 @@ def plot_run( mw, run, subrun1, subrun2, plotfft=True, subbg=True ):
             
         alpha = 0.95
 
-        if len(row)>0:
-            wireid = row['wireid'].values[0]
-            plane = row['plane'].values[0]
-            # FFT
-            if plotfft:
-                
-                red = r['rval']
-                g = r['gval']
-                b = r['bval']
+        # FFT
+        if plotfft:
+            red = r['rval']
+            g = r['gval']
+            b = r['bval']
+            if subbg:
+                red -= r['bg_rval']
+                g -= r['bg_gval']
+                b -= r['bg_bval']
+
+            if red<0:
+                red = 0
+            if g<0:
+                g = 0
+            if b<0:
+                b = 0
+            red /= rmax
+            g /= gmax
+            b /= bmax
+
+            if red>0.1 or g>0.1 or b>0.1 or pulsed:
+                alpha = 0.8
                 if subbg:
-                    red -= r['bg_rval']
-                    g -= r['bg_gval']
-                    b -= r['bg_bval']
-
-                if red<0:
-                    red = 0
-                if g<0:
-                    g = 0
-                if b<0:
-                    b = 0
-                red /= rmax
-                g /= gmax
-                b /= bmax
-                #else:
-                #    mw.vires.setWireColor( plane, wireid, ( 1.0, 1.0, 1.0, 0.1 ) )
-                if red>0.1 or g>0.1 or b>0.1 or pulsed:
-                    alpha = 0.8
-                    if subbg:
-                        print r['crate'],r['slot'],r['femch'],plane,wireid,red,g,b,"bg=(",r['bg_rval']/rmax,r['bg_gval']/gmax,r['bg_bval']/bmax,")"
-                    else:
-                        print r['crate'],r['slot'],r['femch'],plane,wireid,red,g,b
-                    candidates.append( (r['crate'],r['slot'],r['femch']) )
-                # pulsed wire color
-                #if (r['crate'],r['slot'],r['femch'])==(6,9,0):
-                #    mw.vires.setWireColor( plane, wireid, ( 1.0, 1.0, 1.0, 1.0 ) )
-
-                #if above_thresh:
-                mw.vires.setWireColor( plane, wireid, ( (0.1+red), (0.1+g), (0.1+b), alpha ) )
-
-            # AMP
-            else:
-                #if ( r['max_amp']>10.0 ):
-                #    mw.vires.setWireColor( plane, wireid, ( 0.1 + 0.9*r['max_amp']/maxamp, 0.0, 0.0, 1.0 ) )
-                #else:
-                #    mw.vires.setWireColor( plane, wireid, ( 1.0, 1.0, 1.0, 0.1 ) )
-                #red = 0.05 + 0.95*r['max_amp']/maxamp
-                #red = 0.01 + 0.99*(r['max_amp']/r['ped_rms'])/(max_ampratio)
-                #red = 0.01 + 0.99*(r['max_smooth']/r['rms_smooth'])/(max_smooth_ratio)
-                red = 0.01 + 0.99*r['max_amp']/pulsed_maxamp
-                if not pulsed:
-                    if r['max_smooth']/r['rms_smooth']>5.0 and r['max_amp']>7.0:
-                        mw.vires.setWireColor( plane, wireid, ( red, 0.01, 0.01, alpha ) ) 
-                        print r['crate'],r['slot'],r['femch'],plane,wireid,red,r['max_amp'],pulsed
-                    else:
-                        mw.vires.setWireColor( plane, wireid, ( 0.01, 0.01, 0.01, alpha ) ) 
+                    print r['crate'],r['slot'],r['femch'],r['plane'],int(r['wireid']),red,g,b,"bg=(",r['bg_rval']/rmax,r['bg_gval']/gmax,r['bg_bval']/bmax,")"
                 else:
-                    mw.vires.setWireColor( plane, wireid, ( red, red, red, 1.0 ) ) 
-                #if red>0.15:
-                #    print r['crate'],r['slot'],r['femch'],plane,wireid,red,pulsed
+                    print r['crate'],r['slot'],r['femch'],r['plane'],['wireid'],red,g,b
+                candidates.append( (r['crate'],r['slot'],r['femch']) )
+            # pulsed wire color
+            #if (r['crate'],r['slot'],r['femch'])==(6,9,0):
+            #    mw.vires.setWireColor( plane, wireid, ( 1.0, 1.0, 1.0, 1.0 ) )
 
-                # pulsed wire color
-                #if (r['crate'],r['slot'],r['femch'])==(6,9,0):
-                #    mw.vires.setWireColor( plane, wireid, ( 0.0, 1.0, 0.0, 1.0 ) )
+            #if above_thresh:
+            mw.vires.setWireColor( r['plane'], int(r['wireid']), ( (0.1+red), (0.1+g), (0.1+b), alpha ) )
+
+        # AMP
+        else:
+            red = 0.01 + 0.99*r['max_amp']/pulsed_maxamp
+            if not pulsed:
+                if r['max_smooth']/r['rms_smooth']>5.0 and r['max_amp']>1.0:
+                    #mw.vires.setWireColor( r['plane'], int(r['wireid']), ( red, 0.01, 0.01, alpha ) ) 
+                    mw.vires.setWireColorByCSF( int(r['crate']),int(r['slot']),int(r['femch']), ( red, 0.01, 0.01, alpha ) ) 
+                    print r['crate'],r['slot'],r['femch'],r['plane'],int(r['wireid']),red,r['max_amp'],pulsed
+                else:
+                    mw.vires.setWireColorByCSF( int(r['crate']),int(r['slot']),int(r['femch']), ( 0.01, 0.01, 0.01, alpha ) ) 
+                    #mw.vires.setWireColor( r['plane'], int(r['wireid']), ( 0.01, 0.01, 0.01, alpha ) ) 
+            else:
+                print "Pulsed: ",int(r['crate']),int(r['slot']),int(r['femch'])
+                mw.vires.setWireColorByCSF( int(r['crate']),int(r['slot']),int(r['femch']), ( red, red, red, 1.0 ) )
+                #mw.vires.setWireColor( r['plane'], int(r['wireid']), ( red, red, red, 1.0 ) ) 
     print candidates
 mw = getmw()
 mw.vires.show()
